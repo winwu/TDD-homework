bdb935451629fef6b9f48d3d2a73ab0b7b2f551f
WIN.WU 吳怡穎 <WIN.WU@delta.corp>
2016-07-05 19:55:04 +0800

    refactor for productcontroller constructor

--- WebApplicationHomework/ProductController.cs
+++ WebApplicationHomework/ProductController.cs
@@ -18,23 +18,15 @@ namespace WebApplicationHomework
     {
         private List<Product> _prods;
 
-        public ProductController()
+       public ProductController()
         {
             // constructor
-            this._prods = new List<Product>
-            {
-                new Product() { Id = 1, Cost = 1, Revenue = 11, SellPrice = 21},
-                new Product() { Id = 2, Cost = 2, Revenue = 12, SellPrice = 22},
-                new Product() { Id = 3, Cost = 3, Revenue = 13, SellPrice = 23},
-                new Product() { Id = 4, Cost = 4, Revenue = 14, SellPrice = 24},
-                new Product() { Id = 5, Cost = 5, Revenue = 15, SellPrice = 25},
-                new Product() { Id = 6, Cost = 6, Revenue = 16, SellPrice = 26},
-                new Product() { Id = 7, Cost = 7, Revenue = 17, SellPrice = 27},
-                new Product() { Id = 8, Cost = 8, Revenue = 18, SellPrice = 28},
-                new Product() { Id = 9, Cost = 9, Revenue = 19, SellPrice = 29},
-                new Product() { Id = 10, Cost = 10, Revenue = 20, SellPrice = 30},
-                new Product() { Id = 11, Cost = 11, Revenue = 21, SellPrice = 31},
-            };
+            this._prods = new List<Product> {};
+        }
+
+        public ProductController(List<Product> product)
+        {
+            this._prods = product;
         }
 
         public List<int> GetProdByPageSizeAndColumnSum(int PageSize, String ColumnName)
--- WebApplicationHomeworkTests/ProductControllerTests.cs
+++ WebApplicationHomeworkTests/ProductControllerTests.cs
@@ -11,11 +11,27 @@ namespace WebApplicationHomework.Tests
     [TestClass()]
     public class ProductControllerTests
     {
+
         [TestMethod()]
         public void Test_Get_Prod_By_PageSize_3_And_Column_is_Cost_Sum()
         {
             // arrange
-            var target = new ProductController();
+            var tmpProduct = new List<Product>
+            {
+                new Product() { Id = 1, Cost = 1, Revenue = 11, SellPrice = 21},
+                new Product() { Id = 2, Cost = 2, Revenue = 12, SellPrice = 22},
+                new Product() { Id = 3, Cost = 3, Revenue = 13, SellPrice = 23},
+                new Product() { Id = 4, Cost = 4, Revenue = 14, SellPrice = 24},
+                new Product() { Id = 5, Cost = 5, Revenue = 15, SellPrice = 25},
+                new Product() { Id = 6, Cost = 6, Revenue = 16, SellPrice = 26},
+                new Product() { Id = 7, Cost = 7, Revenue = 17, SellPrice = 27},
+                new Product() { Id = 8, Cost = 8, Revenue = 18, SellPrice = 28},
+                new Product() { Id = 9, Cost = 9, Revenue = 19, SellPrice = 29},
+                new Product() { Id = 10, Cost = 10, Revenue = 20, SellPrice = 30},
+                new Product() { Id = 11, Cost = 11, Revenue = 21, SellPrice = 31},
+            };
+
+            var target = new ProductController(tmpProduct);
 
             var expected = new List<int> { 6, 15, 24, 21 };
 
@@ -30,7 +46,22 @@ namespace WebApplicationHomework.Tests
         public void Test_Get_Prod_By_PageSize_4_And_Column_is_Revenue_Sum()
         {
             // arrange
-            var target = new ProductController();
+            var tmpProduct = new List<Product>
+            {
+                new Product() { Id = 1, Cost = 1, Revenue = 11, SellPrice = 21},
+                new Product() { Id = 2, Cost = 2, Revenue = 12, SellPrice = 22},
+                new Product() { Id = 3, Cost = 3, Revenue = 13, SellPrice = 23},
+                new Product() { Id = 4, Cost = 4, Revenue = 14, SellPrice = 24},
+                new Product() { Id = 5, Cost = 5, Revenue = 15, SellPrice = 25},
+                new Product() { Id = 6, Cost = 6, Revenue = 16, SellPrice = 26},
+                new Product() { Id = 7, Cost = 7, Revenue = 17, SellPrice = 27},
+                new Product() { Id = 8, Cost = 8, Revenue = 18, SellPrice = 28},
+                new Product() { Id = 9, Cost = 9, Revenue = 19, SellPrice = 29},
+                new Product() { Id = 10, Cost = 10, Revenue = 20, SellPrice = 30},
+                new Product() { Id = 11, Cost = 11, Revenue = 21, SellPrice = 31},
+            };
+
+            var target = new ProductController(tmpProduct);
 
             var expected = new List<int> { 50, 66, 60 };
 
